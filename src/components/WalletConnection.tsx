65f83aef21473199ced6574b524118baee8affb0
sonhaaa <nguyensonha.hanz@gmail.com>
2022-12-14 23:44:32 +0700

    feat: add wallet connection

---
+++ src/components/WalletConnection.tsx
@@ -0,0 +1,33 @@
+import React, { useMemo } from 'react'
+
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { clusterApiUrl } from '@solana/web3.js'
+
+import '@solana/wallet-adapter-react-ui/styles.css'
+
+const WalletConnection = ({ children }: { children: JSX.Element }) => {
+  const network = WalletAdapterNetwork.Devnet
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter()],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network],
+  )
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
+
+export default WalletConnection
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, redirect, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import WalletConnection from './components/WalletConnection'
 import './index.css'
 import Landing from './pages/Landing'
 import NotFound from './pages/NotFound'
@@ -9,7 +10,11 @@ import StoreBuilder from './pages/StoreBuilder'
 const router = createBrowserRouter([
   {
     path: '/store-builder',
-    element: <StoreBuilder />,
+    element: (
+      <WalletConnection>
+        <StoreBuilder />
+      </WalletConnection>
+    ),
   },
   {
     path: '/',
